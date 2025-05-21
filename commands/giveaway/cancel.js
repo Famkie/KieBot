bff227e1c878248b44aa05d6b9edcc33a5337a8b
Famkie <famkie.id@gmail.com>
2025-05-21 19:59:46 +0700

    Update cancel.js

--- commands/giveaway/cancel.js
+++ commands/giveaway/cancel.js
@@ -1,18 +1,22 @@
+import { SlashCommandBuilder } from 'discord.js';
 
-const { SlashCommandBuilder } = require('discord.js');
-
-module.exports = {
+export default {
   data: new SlashCommandBuilder()
     .setName('cancelgiveaway')
     .setDescription('Batalkan giveaway aktif')
     .addStringOption(opt =>
-      opt.setName('message_id').setDescription('ID pesan giveaway').setRequired(true)
+      opt.setName('message_id')
+        .setDescription('ID pesan giveaway')
+        .setRequired(true)
     ),
-  async execute(interaction) {
+
+  async slashExecute(interaction) {
     const messageId = interaction.options.getString('message_id');
     const giveaway = interaction.client.giveaways.get(messageId);
 
-    if (!giveaway) return interaction.reply({ content: 'Giveaway tidak ditemukan.', ephemeral: true });
+    if (!giveaway) {
+      return interaction.reply({ content: 'Giveaway tidak ditemukan.', ephemeral: true });
+    }
 
     interaction.client.giveaways.delete(messageId);
     interaction.reply({ content: 'Giveaway berhasil dibatalkan.' });
