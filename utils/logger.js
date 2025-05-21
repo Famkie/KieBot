df9c2ab746014088086cd3ca2066c36d0b7b3251
Famkie <famkie.id@gmail.com>
2025-05-21 19:38:28 +0700

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,12 +1,10 @@
 // utils/logger.js
-const chalk = require('chalk');
+import chalk from 'chalk';
 
 const log = {
   info: (...args) => console.log(chalk.blue('[INFO]'), ...args),
   warn: (...args) => console.log(chalk.yellow('[WARN]'), ...args),
   error: (...args) => console.log(chalk.red('[ERROR]'), ...args),
-  event: (...args) => console.log(chalk.magenta('[EVENT]'), ...args),
-  command: (...args) => console.log(chalk.green('[COMMAND]'), ...args),
 };
 
-module.exports = { log };
+export default log;
