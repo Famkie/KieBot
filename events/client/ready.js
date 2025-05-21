e0370e13719be6b3b1fd68f768db118569d9bbc9
Famkie <famkie.id@gmail.com>
2025-05-21 17:32:25 +0700

    Add files via upload

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,1 +1,8 @@
-
+module.exports = {
+  name: 'ready',
+  once: true, // dijalankan sekali saja saat bot nyala
+  execute(client) {
+    console.log(`✅ Bot ${client.user.tag} sudah online!`);
+    client.user.setActivity('/help | KieBot', { type: 'PLAYING' }); // Status bot
+  },
+};
