e9685df2fb8c839adefba1fab81e57392fe5e2e6
Famkie <famkie.id@gmail.com>
2025-05-21 20:16:15 +0700

    Update cd.js

--- commands/lotto/cd.js
+++ commands/lotto/cd.js
@@ -1,29 +1,27 @@
-// commands/lotto/cd.js
-const { SlashCommandBuilder } = require('discord.js');
-const lottoStore = require('../../utils/torn/lottoStore.js');
-const { drawWinners } = require('../../utils/torn/lottoDraw.js');
+import { SlashCommandBuilder } from 'discord.js';
+import lottoStore from '../../utils/torn/lottoStore.js';
+import { drawWinners } from '../../utils/torn/lottoDraw.js';
 
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('cd')
-    .setDescription('Mulai countdown dan draw otomatis setelah waktu tertentu')
-    .addIntegerOption(option =>
-      option.setName('detik')
-        .setDescription('Waktu dalam detik sebelum draw dilakukan')
-        .setRequired(true)),
+export const data = new SlashCommandBuilder()
+  .setName('cd')
+  .setDescription('Mulai countdown dan draw otomatis setelah waktu tertentu')
+  .addIntegerOption(option =>
+    option.setName('detik')
+      .setDescription('Waktu dalam detik sebelum draw dilakukan')
+      .setRequired(true)
+  );
 
-  async execute(interaction) {
-    const detik = interaction.options.getInteger('detik');
-    const lotto = lottoStore.activeLotto;
+export async function execute(interaction) {
+  const detik = interaction.options.getInteger('detik');
+  const lotto = lottoStore.activeLotto;
 
-    if (!lotto) {
-      return interaction.reply('Tidak ada undian yang sedang berjalan.');
-    }
-
-    interaction.reply(`Countdown dimulai. Draw akan dilakukan dalam ${detik} detik.`);
-
-    setTimeout(() => {
-      drawWinners(interaction.client, interaction.channel);
-    }, detik * 1000);
+  if (!lotto) {
+    return interaction.reply('Tidak ada undian yang sedang berjalan.');
   }
-};
+
+  await interaction.reply(`Countdown dimulai. Draw akan dilakukan dalam ${detik} detik.`);
+
+  setTimeout(() => {
+    drawWinners(interaction.client, interaction.channel);
+  }, detik * 1000);
+}
