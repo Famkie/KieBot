a2e55c4bc128d78564fe1661724315e7f508b6b4
Famkie <famkie.id@gmail.com>
2025-05-21 20:36:59 +0700

    Update warn.js

--- commands/moderation/warn.js
+++ commands/moderation/warn.js
@@ -1,21 +1,24 @@
-module.exports = {
+export default {
   name: 'warn',
   description: 'Berikan peringatan ke member.',
   async execute(message, args) {
-    if (!message.member.permissions.has('ManageMessages'))
+    if (!message.member.permissions.has('ManageMessages')) {
       return message.reply("Kamu tidak punya izin untuk memberi peringatan.");
+    }
 
     const member = message.mentions.members.first();
     const alasan = args.slice(1).join(" ") || "Tidak disebutkan";
 
-    if (!member) return message.reply("Mention member yang ingin diberi peringatan.");
+    if (!member) {
+      return message.reply("Mention member yang ingin diberi peringatan.");
+    }
 
     try {
-      await member.send(`Kamu mendapat peringatan dari ${message.guild.name}: ${alasan}`);
-      message.channel.send(`Peringatan telah dikirim ke ${member.user.tag}.`);
+      await member.send(`**Peringatan dari ${message.guild.name}**\nAlasan: ${alasan}`);
+      message.channel.send(`Peringatan telah dikirim ke **${member.user.tag}**.`);
     } catch (err) {
       console.error(err);
-      message.channel.send("Gagal kirim DM ke user.");
+      message.channel.send("Gagal kirim DM ke user. Kemungkinan DM-nya ditutup.");
     }
   }
 };
