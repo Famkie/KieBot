6341fcd304d94f01cba35db0aa07612d6b64890b
Famkie <famkie.id@gmail.com>
2025-05-21 20:30:46 +0700

    Update kick.js

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -1,20 +1,27 @@
-module.exports = {
-  name: 'kick',
-  description: 'Kick seorang member dari server.',
-  execute(message, args) {
-    if (!message.member.permissions.has('KickMembers'))
-      return message.reply("Kamu tidak punya izin untuk kick.");
+import { PermissionsBitField } from 'discord.js';
 
-    const member = message.mentions.members.first();
-    if (!member) return message.reply("Mention user yang ingin kamu kick.");
+export const name = 'kick';
+export const description = 'Kick seorang member dari server.';
 
-    if (!member.kickable) return message.reply("Aku tidak bisa kick user itu!");
-
-    member.kick().then(() => {
-      message.channel.send(`${member.user.tag} telah di-kick.`);
-    }).catch(err => {
-      message.channel.send("Gagal kick user.");
-      console.error(err);
-    });
+export async function execute(message, args) {
+  if (!message.member.permissions.has(PermissionsBitField.Flags.KickMembers)) {
+    return message.reply('Kamu tidak punya izin untuk kick.');
   }
-};
+
+  const member = message.mentions.members.first();
+  if (!member) {
+    return message.reply('Mention user yang ingin kamu kick.');
+  }
+
+  if (!member.kickable) {
+    return message.reply('Aku tidak bisa kick user itu!');
+  }
+
+  try {
+    await member.kick();
+    message.channel.send(`${member.user.tag} telah di-kick.`);
+  } catch (err) {
+    console.error(err);
+    message.channel.send('Gagal kick user.');
+  }
+}
