4d366a14f6dafb7ddbd6f7bfac29c29f9f929db0
Famkie <famkie.id@gmail.com>
2025-05-22 05:27:45 +0700

    Update lottoStore.js

--- utils/torn/lottoStore.js
+++ utils/torn/lottoStore.js
@@ -1,36 +1,40 @@
-// utils/lottoStore.js
-
 let currentLotto = null;
 let lottoEntries = [];
 let lottoStats = {};
 
-module.exports = {
-  setCurrentLotto(data) {
-    currentLotto = data;
-  },
-  getCurrentLotto() {
-    return currentLotto;
-  },
-  clearCurrentLotto() {
-    currentLotto = null;
-    lottoEntries = [];
-  },
-  addEntry(entry) {
-    lottoEntries.push(entry);
-  },
-  getEntries() {
-    return lottoEntries;
-  },
-  getEntryById(id) {
-    return lottoEntries.find(e => e.id === id);
-  },
-  getEntryCount() {
-    return lottoEntries.length;
-  },
-  addStat(userId) {
-    lottoStats[userId] = (lottoStats[userId] || 0) + 1;
-  },
-  getStats() {
-    return lottoStats;
-  }
-};
+export function setCurrentLotto(data) {
+  currentLotto = data;
+}
+
+export function getCurrentLotto() {
+  return currentLotto;
+}
+
+export function clearCurrentLotto() {
+  currentLotto = null;
+  lottoEntries = [];
+}
+
+export function addEntry(entry) {
+  lottoEntries.push(entry);
+}
+
+export function getEntries() {
+  return lottoEntries;
+}
+
+export function getEntryById(id) {
+  return lottoEntries.find(e => e.id === id);
+}
+
+export function getEntryCount() {
+  return lottoEntries.length;
+}
+
+export function addStat(userId) {
+  lottoStats[userId] = (lottoStats[userId] || 0) + 1;
+}
+
+export function getStats() {
+  return lottoStats;
+}
