dbf98ea73583493c5cbeb42e14d077c26f931b8b
Famkie <famkie.id@gmail.com>
2025-05-22 05:18:16 +0700

    Update fetchTorn.js

--- utils/torn/fetchTorn.js
+++ utils/torn/fetchTorn.js
@@ -1,9 +1,14 @@
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 
-async function fetchTornData(endpoint = 'user', selections = 'basic', apiKey) {
+/**
+ * Ambil data dari API Torn City.
+ * @param {string} endpoint - Endpoint Torn API, contoh: 'user'
+ * @param {string} selections - Selections data, contoh: 'basic,profile'
+ * @param {string} apiKey - API key pengguna
+ * @returns {Promise<Object>} - Data JSON dari API
+ */
+export default async function fetchTornData(endpoint = 'user', selections = 'basic', apiKey) {
   const url = `https://api.torn.com/${endpoint}/?selections=${selections}&key=${apiKey}`;
   const res = await fetch(url);
-  return await res.json();
+  return res.json();
 }
-
-module.exports = fetchTornData;
