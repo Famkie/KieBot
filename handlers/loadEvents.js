01cc75501141e757224ed468df83e78a447e8398
Famkie <famkie.id@gmail.com>
2025-05-22 15:48:38 +0700

    Create loadEvents.js

---
+++ handlers/loadEvents.js
@@ -0,0 +1,31 @@
+// handlers/loadEvents.js
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default async (client) => {
+  const eventsPath = path.join(__dirname, '..', 'events');
+  const folders = fs.readdirSync(eventsPath);
+
+  for (const folder of folders) {
+    const folderPath = path.join(eventsPath, folder);
+    const files = fs.readdirSync(folderPath).filter(f => f.endsWith('.js'));
+
+    for (const file of files) {
+      const filePath = path.join(folderPath, file);
+      const eventModule = await import(filePath);
+      const event = eventModule.default;
+
+      if (event?.name && typeof event.execute === 'function') {
+        if (event.once) {
+          client.once(event.name, (...args) => event.execute(client, ...args));
+        } else {
+          client.on(event.name, (...args) => event.execute(client, ...args));
+        }
+      }
+    }
+  }
+};
