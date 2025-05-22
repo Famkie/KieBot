df0ee9d31a5334780c6fad3e39b15aa754857ecb
Famkie <famkie.id@gmail.com>
2025-05-22 14:57:11 +0700

    Update ping.js

--- commands/prefix/general/ping.js
+++ commands/prefix/general/ping.js
@@ -1,1 +1,14 @@
+// commands/prefix/general/ping.js
+export const name = 'ping';
+export const description = 'Cek latency bot.';
 
+export async function execute(message, args) {
+  try {
+    const sent = await message.channel.send('Pinging...');
+    const latency = sent.createdTimestamp - message.createdTimestamp;
+    await sent.edit(`Pong! Latency: ${latency}ms`);
+  } catch (err) {
+    console.error('Ping error:', err);
+    message.channel.send('Gagal melakukan ping.');
+  }
+} 
