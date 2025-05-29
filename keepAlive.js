c2b3dc17eea32c6e9485164be2f9fe6907fb03c9
Famkie <famkie.id@gmail.com>
2025-05-29 16:20:17 +0700

    Update keepAlive.js

--- keepAlive.js
+++ keepAlive.js
@@ -1,1 +1,14 @@
-// keepAlive.js placeholder
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot hidup!");
+});
+
+function keepAlive() {
+  app.listen(3000, () => {
+    console.log("Keep-alive server jalan di port 3000");
+  });
+}
+
+module.exports = keepAlive;
